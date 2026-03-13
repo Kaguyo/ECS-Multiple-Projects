fdf91b09e3defc00c2d13598d04b60b66d93765a
Gabriel Silverio <gabrielsilverio@frigelar.com.br>
2026-03-13 15:11:44 -0300

    --s add todo

--- ecs-sfml-engine/main.cpp
+++ ecs-sfml-engine/main.cpp
@@ -4,4 +4,25 @@ int main() {
     Application app;
     app.run();
     return 0;
+    Cenários não cobertos:
+Erros durante execução de autosserviço
+- retry
+- serviço de email barramento
+
+Parametrização de algumas variáveis de ambiente importantes pro funcionamento. ex: Retry devido erros em auto serviço e finalmente email
+Mudar parâmetro de sleep para segundos/minutos em vez de semanas
+
+rodar go como admin no sudo
+
+crom job
+lock distribuído
+
+teste 1
+teste 4 - dados retornados vazios do Redis com 200
+teste 3
+
+** Redis - Chave - chance de conflito por nomenclatura muito comum
+
+teste 4 GET
+erro - retornando 500 em vez de 404
 }
